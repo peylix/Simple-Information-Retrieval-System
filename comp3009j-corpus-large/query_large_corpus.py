54d803fd263b3a160094120e581452604e6729b0
Peylics <lisichen_2901@outlook.com>
2024-05-30 21:29:35 +0800

    Ported the querying code to the large corpus

---
+++ comp3009j-corpus-large/query_large_corpus.py
@@ -0,0 +1,235 @@
+#!/usr/bin/env python3
+
+import os
+import string
+import sys
+import json
+import time
+
+from collections import Counter
+from files import porter
+
+
+def get_path_of(file_name: str, ignore_existence: bool = False) -> str:
+    '''
+    This function is for acquiring the file path of the given file name.
+    It will get the path from the command line arguments if the arguments follow the correct format.
+    If the file does not exist or the arguments are not in the correct format, 
+    the program will exit with an error message.
+
+    Args:
+    file_name (str): the name of the file (may also includes part of the path).
+    ignore_existence (bool): a flag to ignore the existence of the file (default False), can be used for writing files.
+
+    Returns:
+    str: the file path.
+    '''
+    if len(sys.argv) == 5 and sys.argv[3] == '-p':
+        path = sys.argv[4]
+
+        if os.path.exists(path):
+            file_path = os.path.join(path, file_name)
+
+            if ignore_existence:
+                return file_path
+            elif os.path.exists(file_path):
+                return file_path
+            else:
+                print('Error: The file does not exist. The current recognized file path is ', file_path)
+                sys.exit(1)
+        else:
+            print('Error: The path does not exist.')
+            sys.exit(1)
+    else:
+        print('Error: Invalid arguments.')
+        print('You can use `./query_small_corpus.py -m <mode> -p /path/to/comp3009j-corpus-small` to run the program.')
+        sys.exit(1)
+
+
+def get_mode() -> str:
+    '''
+    This function gets the mode from the command line arguments.
+    If the mode is not in the mode list or the arguments are not in the correct format, 
+    the program will exit with an error message.
+
+    Returns:
+    str: the current mode.
+    '''
+    mode_list = ['interactive', 'automatic']
+    if len(sys.argv) == 5 and sys.argv[1] == '-m':
+        mode = sys.argv[2]
+        if mode in mode_list:
+            return mode
+        else:
+            print('Error: Invalid mode.')
+            print('The available modes are: ', ', '.join(mode_list))
+            sys.exit(1)
+    else:
+        print('Error: Invalid arguments.')
+        print('You can use `./query_small_corpus.py -m <mode> -p /path/to/comp3009j-corpus-small` to run the program.')
+        sys.exit(1)
+
+
+def process_query(stopwords:list, query: str) -> list:
+    '''
+    This function is for conducting several preprocessing steps on the query.
+    The steps include removing punctuation, stopwords, and stemming.
+
+    Args:
+    query (str): the query.
+
+    Returns:
+    list: a list containing the query words.
+    '''
+
+    p = porter.PorterStemmer()
+    result = []
+    stopwords_set = set(stopwords)
+    translation_table = str.maketrans('', '', string.punctuation)
+
+    for word in query.split():
+        # Remove punctuation using the translation table
+        # And convert the word to its lowercase
+        word = word.lower().translate(translation_table)
+        
+        # Remove stopwords and check if the word is not empty
+        if word and word not in stopwords_set:
+            # Stem the word
+            stemmed_word = p.stem(word)
+            # Add the stemmed word to the result
+            result.append(stemmed_word)
+
+    return result
+
+
+def find_relevant_documents(index: dict, processed_query: list) -> dict:
+    '''
+    This function is for finding the relevant documents according to the given query.
+    Since the BM25 scores are already computed, we only need to find the documents that contain the query words 
+    and do some simple processing.
+    I use Counter to store the BM25 scores to avoid duplicates and boost the performance as well as make the code neater.
+
+    Args:
+    index (dict): a dictionary containing the terms with the document IDs and their BM25 scores regarding the term.
+    processed_query (list): a list containing the processed query words.
+
+    Returns:
+    dict: a dictionary containing the relevant document IDs and their BM25 similarity scores.
+    '''
+    result = Counter() # Using Counter to store the BM25 scores to avoid duplicates
+
+    for query_word in processed_query:
+        if query_word in index:
+            result.update(index[query_word])
+
+    
+    # Sort the result by the BM25 score
+    sorted_result = dict(result.most_common())
+
+    return sorted_result
+
+
+# def find_relevant_documents_ignore_duplicate(documents: dict, processed_query: list) -> dict:
+#     '''
+#     This function is for finding the relevant documents according to the given query.
+#     Since the BM25 scores are already computed, we only need to find the documents that contain the query words 
+#     and do some simple processing.
+
+#     Args:
+#     documents (dict): a dictionary containing the terms with the document IDs and their BM25 scores regarding the term.
+#     processed_query (list): a list containing the processed query words.
+
+#     Returns:
+#     dict: a dictionary containing the relevant document IDs and their BM25 similarity scores.
+#     '''
+#     result = Counter()
+#     seen_words = set()  # Set to track processed query words
+    
+#     for query_word in processed_query:
+#         if query_word not in seen_words and query_word in documents:
+#             seen_words.add(query_word)
+#             result.update(documents[query_word])
+    
+#     # Sort the result by the BM25 score
+#     sorted_result = dict(result.most_common())
+#     return sorted_result
+
+
+def format_output(current_query_number: int, relevant_documents: dict, mode: str) -> str:
+    '''
+    This function is for adjusting the relevant documents to make it align with the required format.
+
+    Args:
+    relevant_documents (dict): a dictionary containing the relevant document IDs and their BM25 similarity scores.
+
+    Returns:
+    str: the formatted output.
+    '''
+    result = ''
+    rank_count = 1
+    if mode == 'automatic':
+        for document_id, score in relevant_documents.items():
+            result += f'{current_query_number} {document_id} {rank_count} {score}\n'
+            rank_count += 1
+    elif mode == 'interactive':
+        for document_id, score in relevant_documents.items():
+            result += f'{rank_count} {document_id} {score}\n'
+            rank_count += 1
+            if rank_count > 15:
+                break
+    return result
+    
+
+
+if __name__ == '__main__':
+    start_time = time.process_time()
+    print("+----------Start querying...----------+")
+
+    mode = get_mode()
+    path = get_path_of('documents')
+
+    # Load the stopwords
+    with open(get_path_of('files/stopwords.txt'), 'r') as file:
+        stopwords = [word.strip() for word in file.readlines()]
+
+    # Load the index
+    with open(get_path_of('21207464-large.index'), 'r') as file:
+        built_index = json.load(file)
+    current_time = time.process_time()
+    print(f'{len(built_index)} terms are loaded in {current_time - start_time} seconds.')
+
+    if mode == 'automatic':
+        print('**You are using the automatic mode. The queries will be carried out automatically.**')
+        queries = {}
+        # TODO Use one loop to read the queries and write the results
+        with open(get_path_of('files/queries.txt'), 'r') as file:
+            for line in file:
+                line = line.strip()
+                # Split the line into two parts
+                # The first part is the query ID (key) and the second part is the query (value)
+                query_id, query = line.split(maxsplit=1)
+                queries[query_id] = query
+        current_time = time.process_time()
+        print(f'{len(queries)} queries are loaded in {current_time - start_time} seconds.')
+
+        with open(get_path_of('21207464-large.results', ignore_existence=True), 'w') as file:
+            for query_id, query in queries.items():
+                processed_query = process_query(stopwords, query)
+                relevant_documents = find_relevant_documents(built_index, processed_query)
+                file.write(format_output(int(query_id), relevant_documents, mode='automatic'))
+
+        end_time = time.process_time()
+        print(f'The results are computed in {end_time - start_time} seconds.')
+    elif mode == 'interactive':
+        print('**You are using the interactive mode. Enter "QUIT" to exit.**')
+        while True:
+            query = input('> Please enter your query: ')
+            if query == 'QUIT':
+                break
+            processed_query = process_query(stopwords, query)
+            relevant_documents = find_relevant_documents(built_index, processed_query)
+            print(format_output(1, relevant_documents, mode='interactive'))
+            current_time = time.process_time()
+            print(f'This query is processed in {current_time - start_time} seconds.')
+    
+    print("+----------Querying Ended.----------+")
